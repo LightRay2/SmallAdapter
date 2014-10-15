04ca9ddd862ba4693b547a57f66ae36db141fffe
Zemlyanuhin Mihail <abc-0-4@yandex.ru>
2014-10-15 22:37:27 +0400

    init

---
+++ app/src/main/java/ru/abc_0_4yandex/adapter/ObjectItem.java
@@ -0,0 +1,19 @@
+package ru.abc_0_4yandex.adapter;
+
+/**
+ * Created by Миша on 14.10.2014.
+ */
+
+//another class to handle item's id and name
+public class ObjectItem {
+
+    public int itemId;
+    public String itemName;
+
+    // constructor
+    public ObjectItem(int itemId, String itemName) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+    }
+
+}
